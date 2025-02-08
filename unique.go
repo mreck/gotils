5c834b70c00f04ba68c4126892dd6535753b56f8
Mathias Reck <mathias.reck@gmail.com>
2025-02-08 14:52:13 +0100

    Add unique functions that work for any value

--- unique.go
+++ unique.go
@@ -1,6 +1,6 @@
 package gotils
 
-// MakeSliceUnique returns a new slice containing the first appearance of each value.
+// MakeSliceUnique removes all but the first appearance of each value.
 func MakeSliceUnique[T comparable](array *[]T) {
 	if len(*array) == 0 {
 		return
@@ -22,7 +22,31 @@ func MakeSliceUnique[T comparable](array *[]T) {
 	*array = (*array)[0:next]
 }
 
-// MakeSortedSliceUnique returns a new slice containing the first appearance of each value.
+// MakeSliceUniqueFunc removes all but the first appearance of each value.
+// Values are hashed using the hasher function.
+func MakeSliceUniqueFunc[T any](array *[]T, hasher func(v T) string) {
+	if len(*array) == 0 {
+		return
+	}
+
+	seen := map[string]struct{}{}
+	seen[hasher((*array)[0])] = struct{}{}
+
+	next := 1
+	for i := 1; i < len(*array); i++ {
+		v := (*array)[i]
+		h := hasher(v)
+		if _, ok := seen[h]; !ok {
+			(*array)[next] = v
+			next = next + 1
+			seen[h] = struct{}{}
+		}
+	}
+
+	*array = (*array)[0:next]
+}
+
+// MakeSortedSliceUnique removes all but the first appearance of each value for pre-sorted slices.
 func MakeSortedSliceUnique[T comparable](array *[]T) {
 	if len(*array) == 0 {
 		return
@@ -33,6 +57,26 @@ func MakeSortedSliceUnique[T comparable](array *[]T) {
 		v := (*array)[i]
 		p := (*array)[next-1]
 		if v != p {
+			(*array)[next] = v
+			next = next + 1
+		}
+	}
+
+	*array = (*array)[0:next]
+}
+
+// MakeSortedSliceUnique removes all but the first appearance of each value for pre-sorted slices.
+// Values are compared using the equal function.
+func MakeSortedSliceUniqueFunc[T any](array *[]T, equal func(a, b T) bool) {
+	if len(*array) == 0 {
+		return
+	}
+
+	next := 1
+	for i := 1; i < len(*array); i++ {
+		v := (*array)[i]
+		p := (*array)[next-1]
+		if !equal(v, p) {
 			(*array)[next] = v
 			next = next + 1
 		}
--- unique_test.go
+++ unique_test.go
@@ -1,6 +1,8 @@
 package gotils
 
 import (
+	"fmt"
+	"strings"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
@@ -26,6 +28,28 @@ func TestMakeSliceUnique(t *testing.T) {
 	assert.Equal(t, []int{3, 2, 1}, d)
 }
 
+func TestMakeSliceUniqueFunc(t *testing.T) {
+	var d []int
+
+	hasher := func(v int) string { return fmt.Sprintf("%d", v) }
+
+	d = []int{1, 2, 3}
+	MakeSliceUniqueFunc(&d, hasher)
+	assert.Equal(t, []int{1, 2, 3}, d)
+
+	d = []int{1, 2, 3, 2}
+	MakeSliceUniqueFunc(&d, hasher)
+	assert.Equal(t, []int{1, 2, 3}, d)
+
+	d = []int{1, 2, 3, 1, 2, 3}
+	MakeSliceUniqueFunc(&d, hasher)
+	assert.Equal(t, []int{1, 2, 3}, d)
+
+	d = []int{3, 2, 1, 3, 2, 1}
+	MakeSliceUniqueFunc(&d, hasher)
+	assert.Equal(t, []int{3, 2, 1}, d)
+}
+
 func TestMakeSortedSliceUnique(t *testing.T) {
 	var d []int
 
@@ -40,4 +64,16 @@ func TestMakeSortedSliceUnique(t *testing.T) {
 	d = []int{1, 2, 2, 3, 2, 2}
 	MakeSortedSliceUnique(&d)
 	assert.Equal(t, []int{1, 2, 3, 2}, d)
+}
+
+func TestMakeSortedSliceUniqueFunc(t *testing.T) {
+	var d []string
+
+	d = []string{"a", "A", "b"}
+	MakeSortedSliceUniqueFunc(&d, strings.EqualFold)
+	assert.Equal(t, []string{"a", "b"}, d)
+
+	d = []string{"A", "a", "b"}
+	MakeSortedSliceUniqueFunc(&d, strings.EqualFold)
+	assert.Equal(t, []string{"A", "b"}, d)
 }
