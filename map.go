91cf6c63ed334f103cec84697232448574445402
Mathias Reck <mathias.reck@gmail.com>
2025-04-07 00:09:12 +0200

    Add CopyMap()

--- map.go
+++ map.go
@@ -1,5 +1,9 @@
 package gotils
 
+import (
+	"maps"
+)
+
 func GetMapKeys[K comparable, V any](m map[K]V) []K {
 	r := make([]K, 0, len(m))
 	for k := range m {
@@ -14,4 +18,10 @@ func GetMapValues[K comparable, V any](m map[K]V) []V {
 		r = append(r, v)
 	}
 	return r
+}
+
+func CopyMap[K comparable, V any](m map[K]V) map[K]V {
+	m2 := map[K]V{}
+	maps.Copy(m2, m)
+	return m2
 }
--- map_test.go
+++ map_test.go
@@ -21,3 +21,11 @@ func TestGetMapValues(t *testing.T) {
 	sort.Ints(k)
 	assert.Equal(t, []int{2, 4, 6}, k)
 }
+
+func TestCopyMap(t *testing.T) {
+	m1 := map[int]int{1: 2, 3: 4, 5: 6}
+	m2 := gotils.CopyMap(m1)
+	assert.Equal(t, m1, m2)
+	m1[1] = 7
+	assert.Equal(t, 2, m2[1])
+}
