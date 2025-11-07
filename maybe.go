16e65038b293fb949bb19f87557ed0b02ec52fe7
Mathias Reck <mathias.reck@gmail.com>
2025-11-07 15:24:07 +0100

    Simplify Maybe[T]

--- maybe.go
+++ maybe.go
@@ -1,34 +1,7 @@
 package gotils
 
+// Maybe wraps a value and adds validity
 type Maybe[T any] struct {
-	value T
-	isSet bool
-}
-
-func EmptyMaybe[T any]() Maybe[T] {
-	var m Maybe[T]
-	return m
-}
-
-func NewMaybe[T any](value T) Maybe[T] {
-	return Maybe[T]{value, true}
-}
-
-func (m Maybe[T]) IsSet() bool {
-	return m.isSet
-}
-
-func (m Maybe[T]) Get() (T, bool) {
-	return m.value, m.isSet
-}
-
-func (m *Maybe[T]) Set(value T) {
-	m.value = value
-	m.isSet = true
-}
-
-func (m *Maybe[T]) Unset() {
-	var v T
-	m.value = v
-	m.isSet = false
+	Value T
+	Valid bool
 }
