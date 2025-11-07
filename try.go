7cfec18c3e98bcbdc169b023b2244ab1c9324caa
Mathias Reck <mathias.reck@gmail.com>
2025-11-07 15:18:04 +0100

    Improve try functions

--- try.go
+++ try.go
@@ -1,20 +1,51 @@
 package gotils
 
 import (
+	"context"
 	"time"
 )
 
 // Try reruns the function until it succeeds or runs out of attempts
-func Try(noAttempts int, waitDuration time.Duration, f func(attempt int) error) error {
+func Try(ctx context.Context, maxAttempts int, waitDuration time.Duration, f func(ctx context.Context, attempt int) error) error {
 	var err error
 
-	for a := range noAttempts {
-		err = f(a)
+	for attempt := range maxAttempts {
+		err = ctx.Err()
+		if err != nil {
+			break
+		}
+
+		err = f(ctx, attempt)
 		if err == nil {
 			break
 		}
+
 		time.Sleep(waitDuration)
 	}
 
 	return err
+}
+
+// Try2 reruns the function until it succeeds or runs out of attempts
+func Try2[T any](ctx context.Context, maxAttempts int, waitDuration time.Duration, f func(ctx context.Context, attempt int) (T, error)) (T, error) {
+	var (
+		val T
+		err error
+	)
+
+	for attempt := range maxAttempts {
+		err = ctx.Err()
+		if err != nil {
+			break
+		}
+
+		val, err = f(ctx, attempt)
+		if err == nil {
+			break
+		}
+
+		time.Sleep(waitDuration)
+	}
+
+	return val, err
 }
--- try_test.go
+++ try_test.go
@@ -1,24 +1,73 @@
 package gotils_test
 
 import (
+	"context"
 	"fmt"
 	"testing"
 
 	"github.com/mreck/gotils"
+
 	"github.com/stretchr/testify/assert"
 )
 
 func Test_Try(t *testing.T) {
-	err := gotils.Try(3, 0, func(attempt int) error {
+	ctx := context.Background()
+
+	err := gotils.Try(ctx, 3, 0, func(ctx context.Context, attempt int) error {
 		return fmt.Errorf("test")
 	})
+
 	assert.Error(t, err)
 
-	err = gotils.Try(3, 0, func(attempt int) error {
+	err = gotils.Try(ctx, 3, 0, func(ctx context.Context, attempt int) error {
 		if attempt < 2 {
 			return fmt.Errorf("test")
 		}
 		return nil
 	})
+
 	assert.NoError(t, err)
+
+	ctx, cancel := context.WithCancel(ctx)
+	cancel()
+
+	err = gotils.Try(ctx, 3, 0, func(ctx context.Context, attempt int) error {
+		return nil
+	})
+
+	assert.ErrorIs(t, err, context.Canceled)
+}
+
+func Test_Try2(t *testing.T) {
+	ctx := context.Background()
+
+	n, err := gotils.Try2(ctx, 3, 0, func(ctx context.Context, attempt int) (int, error) {
+		return attempt, fmt.Errorf("test")
+	})
+
+	assert.Error(t, err)
+	assert.Equal(t, 2, n)
+
+	n, err = gotils.Try2(ctx, 3, 0, func(ctx context.Context, attempt int) (int, error) {
+		if attempt < 1 {
+			return attempt, fmt.Errorf("test")
+		}
+		return attempt, nil
+	})
+
+	assert.NoError(t, err)
+	assert.Equal(t, 1, n)
+
+	ctx, cancel := context.WithCancel(ctx)
+	cancel()
+
+	n, err = gotils.Try2(ctx, 3, 0, func(ctx context.Context, attempt int) (int, error) {
+		if attempt < 1 {
+			return attempt, fmt.Errorf("test")
+		}
+		return attempt, nil
+	})
+
+	assert.ErrorIs(t, err, context.Canceled)
+	assert.Equal(t, 0, n)
 }
